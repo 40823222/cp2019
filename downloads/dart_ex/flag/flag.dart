39844225b6ed682f04122f05cf88936deb8ff927
40823222 <40823222@mde.tw>
2020-01-02 16:02:20 +0800

    w17

--- downloads/dart_ex/flag/flag.dart
+++ downloads/dart_ex/flag/flag.dart
@@ -5,216 +5,22 @@ CanvasElement canvas;
 CanvasRenderingContext2D ctx;
 int flag_w = 300;
 int flag_h = 200;
-num circle_x = flag_w / 2;
-num circle_y = flag_h / 2;
+num circle_x = flag_w / 4;
+num circle_y = flag_h / 4;
 
-void main() {
-  canvas = querySelector('#canvas');
-  ctx = canvas.getContext('2d');
 
-  drawROC(ctx);
-  querySelector("#germany").onClick.listen((e) => drawGermany(ctx));
-  querySelector("#netherlands").onClick.listen((e) => drawNetherlands(ctx));
-  querySelector("#uk").onClick.listen((e) => drawUK(ctx));
-  querySelector("#russia").onClick.listen((e) => drawRussia(ctx));
-  querySelector("#france").onClick.listen((e) => drawFrance(ctx));
-  querySelector("#roc").onClick.listen((e) => drawROC(ctx));
-  querySelector("#jp").onClick.listen((e) => drawJP(ctx));
-  querySelector("#usa").onClick.listen((e) => drawUSA(ctx));
-  querySelector("#button").onClick.listen((e) => clearCanvas());
-}
-void drawJP(ctx){
-  ctx.clearRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(0, 0, flag_w, flag_h);
-  ctx.beginPath();
-  ctx.arc(circle_x, circle_y, 60, 0, Math.pi * 2, true);
-  ctx.closePath();
-  // 填色設為白色
-  ctx.fillStyle = 'rgb(188,0,45)';
-  ctx.fill();
-  ctx.clearRect(300,0, 200, 300);
-}
-num circle_a = flag_w / 4;
-num circle_b = flag_h / 4;
-void drawROC(ctx){
-  // 先畫滿地紅
-  ctx.clearRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(255, 0, 0)';
-  ctx.fillRect(0, 0, flag_w, flag_h);
-  // 再畫青天
-  ctx.fillStyle = 'rgb(0, 0, 150)';
-  ctx.fillRect(0, 0, flag_w / 2, flag_h / 2);
-  // 畫十二道光芒白日
-  ctx.beginPath();
-  num star_radius = flag_w / 8;
-  num angle = 0;
-  for (int i = 0; i < 25; i++) {
-    angle += 5 * Math.pi * 2 / 12;
-    num toX = circle_a + Math.cos(angle) * star_radius;
-    num toY = circle_b + Math.sin(angle) * star_radius;
-    // 只有 i 為 0 時移動到 toX, toY, 其餘都進行 lineTo
-    if (i != 0)
-      ctx.lineTo(toX, toY);
-    else
-      ctx.moveTo(toX, toY);
-  }
-  ctx.closePath();
-  // 將填色設為白色
-  ctx.fillStyle = '#fff';
-  ctx.fill();
-  // 白日:藍圈
-  ctx.beginPath();
-  ctx.arc(circle_a, circle_b, flag_w * 17 / 240, 0, Math.pi * 2, true);
-  ctx.closePath();
-  // 填色設為藍色
-  ctx.fillStyle = 'rgb(0, 0, 149)';
-  ctx.fill();
-  // 白日:白心
-  ctx.beginPath();
-  ctx.arc(circle_a, circle_b, flag_w / 16, 0, Math.pi * 2, true);
-  ctx.closePath();
-  // 填色設為白色
-  ctx.fillStyle = '#fff';
-  ctx.fill();
-  ctx.clearRect(300,0, 200, 300);
-}
-void drawFrance(ctx){
-  ctx.clearRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(250,60,50)';
-  ctx.fillRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(0, 0,flag_w*63/100, flag_h);
-  ctx.fillStyle = 'rgb(0,85,164)';
-  ctx.fillRect(0, 0,flag_w*30/100, flag_h);
-  ctx.clearRect(300,0, 200, 300);
-  
-}
-void drawNetherlands(ctx) {
-  ctx.clearRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(174,28,40)';
-  ctx.fillRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(0, flag_h/3, flag_w, flag_h/3);
-  ctx.fillStyle = 'rgb(33, 70, 139)';
-  ctx.fillRect(0, flag_h*2/3, flag_w, flag_h/3);
-  ctx.clearRect(300,0, 200, 300);
-}
-  
-void drawUK(ctx){
-  ctx.clearRect(0, 0, 300, 150);
-  ctx.fillStyle = 'rgb(0, 36, 125)';
-  ctx.fillRect(0, 0, 300, 150);
-  ctx.beginPath();
-  ctx.save();
-  ctx.translate(0,0);
-  ctx.rotate(26*Math.pi/180);
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(0,-13.5, 400, 30);
-  ctx.restore();
-  ctx.fillStyle = '#fff';
-  ctx.fill();
-  ctx.beginPath();
-  ctx.save();
-  ctx.translate(0,0);
-  ctx.rotate(-26*Math.pi/180);
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(-70, 118, 400, 30);
-  ctx.restore();
-  ctx.fillStyle = '#fff';
-  ctx.fill();
-  ctx.beginPath();
-  ctx.save();
-  ctx.translate(0,0);
-  ctx.rotate(26*Math.pi/180);
-  ctx.fillStyle = 'rgb(207, 20, 43)';
-  ctx.fillRect(-50, 0, 200, 10);
-  ctx.restore();
-  ctx.fillStyle = '#fff';
-  ctx.fill();
-  ctx.beginPath();
-  ctx.save();
-  ctx.translate(0,0);
-  ctx.rotate(26*Math.pi/180);
-  ctx.fillStyle = 'rgb(207, 20, 43)';
-  ctx.fillRect(190, -7, 200, 10);
-  ctx.restore();
-  ctx.fillStyle = '#fff';
-  ctx.fill();
-  ctx.beginPath();
-  ctx.save();
-  ctx.translate(0,0);
-  ctx.rotate(-26*Math.pi/180);
-  ctx.fillStyle = 'rgb(207, 20, 43)';
-  ctx.fillRect(-120, 132.5, 200, 10);
-  ctx.restore();
-  ctx.fillStyle = '#fff';
-  ctx.fill();
-  ctx.beginPath();
-  ctx.save();
-  ctx.translate(0,0);
-  ctx.rotate(-26*Math.pi/180);
-  ctx.fillStyle = 'rgb(207, 20, 43)';
-  ctx.fillRect(120,123.5, 200, 10);
-  ctx.restore();
-  ctx.fillStyle = '#fff';
-  ctx.fill();
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(125,0, 50, 150);
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(0,50, 300, 50);
-  ctx.fillStyle = 'rgb(207, 20, 43)';
-  ctx.fillRect(0, 60, 300, 30);
-  ctx.fillStyle = 'rgb(207, 20, 43)';
-  ctx.fillRect(135, 0, 30,150 );
-  ctx.clearRect(300,0, 200, 300);
-  ctx.clearRect(0,150, 400, 300);
-}
-void drawRussia(ctx){
-  ctx.clearRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(225,37,29)';
-  ctx.fillRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(0,61,165)';
-  ctx.fillRect(0, 0,flag_w, flag_h*2/3);
-  ctx.fillStyle = 'rgb(255,255,255)';
-  ctx.fillRect(0, 0,flag_w, flag_h*1/3);
-  ctx.clearRect(300,0, 200, 300);
-}
-void drawGermany(ctx){
-  ctx.clearRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(255,204,0)';
-  ctx.fillRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(221,0,0)';
-  ctx.fillRect(0, 0,flag_w, flag_h*2/3);
-  ctx.fillStyle = 'rgb(0,0,0)';
-  ctx.fillRect(0, 0,flag_w, flag_h*1/3);
-  ctx.clearRect(300,0, 200, 300);
-}
-int flag_wi = 380;
-int flag_he = 200;
-
-fivePointStar(x, y, r, solid, theta, color) {
-  // 五個圓周外點數列
+ fivePointStar(x, y, r, solid, theta, color) {
   List outerPoints = [];
-  // 五個圓周內點數列
   List innerPoints = [];
-  // 用於將角度轉為徑度的轉換子
   var degree = Math.pi / 180;
   var innerR = r * Math.cos(72 * degree) / Math.cos(36 * degree);
-  // 假如在 html 模式下執行非超文件內容相關的 print, 資料將會顯示在 console 區
   print("準備畫一個位於 ($x, $y), 半徑 $r, 實心為 $solid, 顏色為 $color 的五芒星");
-  // 開始設定 path
   ctx.beginPath();
-  // 先設法畫一個圓心位於 x, y, 半徑為 r 的圓
-  //ctx.arc(x, y, r, 0, 2 * Math.pi);
-  // 沿用既有的座標系統, x 向右為正, y 向下為正
-  // 利用 for 迴圈求五芒星的五個外點座標
   for (int i = 0; i <= 5; i++) {
     outerPoints.add([
       x + r * Math.sin(72 * degree * i + theta * degree),
       y - r * Math.cos(72 * degree * i + theta * degree)
     ]);
-    // 內點數列, 以 innerR 為半徑, 但是角度額外轉 -36 度
     innerPoints.add([
       x +
           innerR *
@@ -237,108 +43,370 @@ fivePointStar(x, y, r, solid, theta, color) {
   }
   ctx.closePath();
   if (solid == false) {
-    // 根據所設定的 color 值設定 strokeStyle
     ctx.strokeStyle = color;
-    // path 要執行到 stroke() 方法才會下筆繪圖
     ctx.stroke();
   } else {
     ctx.fillStyle = color;
     ctx.fill();
   }
 }
-fivePointStar2(a, b, R, solid, theta, color) {
-  // 五個圓周外點數列
-  List outerPoints = [];
-  // 五個圓周內點數列
-  List innerPoints = [];
-  // 用於將角度轉為徑度的轉換子
-  var degree = Math.pi / 180;
-  var innerR = R * Math.cos(72 * degree) / Math.cos(36 * degree);
-  // 假如在 html 模式下執行非超文件內容相關的 print, 資料將會顯示在 console 區
-  print("準備畫一個位於 ($a, $b), 半徑 $R, 實心為 $solid, 顏色為 $color 的五芒星");
-  // 開始設定 path
-  ctx.beginPath();
-  // 先設法畫一個圓心位於 x, y, 半徑為 r 的圓
-  //ctx.arc(x, y, r, 0, 2 * Math.pi);
-  // 沿用既有的座標系統, x 向右為正, y 向下為正
-  // 利用 for 迴圈求五芒星的五個外點座標
-  for (int i = 0; i <= 5; i++) {
-    outerPoints.add([
-      a + R * Math.sin(72 * degree * i + theta * degree),
-      b - R * Math.cos(72 * degree * i + theta * degree)
-    ]);
-    // 內點數列, 以 innerR 為半徑, 但是角度額外轉 -36 度
-    innerPoints.add([
-      a +
-          innerR *
-              Math.sin(72 * degree * i +
-                  theta * degree -
-                  36 * degree),
-      b -
-          innerR *
-              Math.cos(72 * degree * i +
-                  theta * degree -
-                  36 * degree)
-    ]);
-  }
-  print(outerPoints);
-  ctx.moveTo(outerPoints[0][0], outerPoints[0][1]);
-  for (int i = 1; i < 5; i++) {
-    ctx.lineTo(innerPoints[i][0], innerPoints[i][1]);
-    ctx.lineTo(outerPoints[i][0], outerPoints[i][1]);
-    ctx.lineTo(innerPoints[i + 1][0], innerPoints[i + 1][1]);
-  }
-  ctx.closePath();
-  if (solid == false) {
-    // 根據所設定的 color 值設定 strokeStyle
-    ctx.strokeStyle = color;
-    // path 要執行到 stroke() 方法才會下筆繪圖
-    ctx.stroke();
-  } else {
-    ctx.fillStyle = color;
-    ctx.fill();
-  }
-}
-void drawUSA(ctx) {
-  ctx.clearRect(0, 0, flag_wi, flag_he);
-  ctx.fillStyle = 'rgb(191,10,48)';
-  ctx.fillRect(0, 0, flag_wi, flag_he);
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  for (int i = 1; i <=11; i+=2) {
-  ctx.fillRect(0, 15.38*i,flag_wi, 15.38);
-  }
-  ctx.fillStyle = 'rgb(0,40,104)';
-  ctx.fillRect(0, 0, 152,107.7);
 
-  // 五芒星圓心為 (x, y)
-  double x = 12.66;
-  double y = 10.76;
-  // 五芒星半徑為 r
-  double r = 6.16;
-  // 若 solid 為 false, 表示畫空心五芒星, true 為著色五芒星
-  bool solid = true;
-  // 旋轉角度設為 0 度
-  var theta = 0;
-  // 因為背景為暗色, 以 white 白色作圖
-  var color = "white";
-  // 呼叫 fivePointStar() 函式
-  int i , j ;
-  for (i = 0; i < 6; i++) {
-       for(j = 0; j <= 8;j++) {
-         if(j%2==0)
-  fivePointStar(x+i*2*x, y+j*y, r, solid, theta, color);
-  }
-  }
-  double a =12.66*2;
-  double b =10.76*2;
-  double R =6.16;
-  for (i = 0; i < 5; i++) {
-       for(j = 0; j <= 6;j++) {
-         if(j%2==0)
-  fivePointStar(a+i*2*x, b+j*y, R, solid, theta, color);
-  }
-  }
+
+void main() {
+  canvas = querySelector('#canvas');
+  ctx = canvas.getContext('2d');
+  
+ 
+
+  drawCuba(ctx);
+  querySelector("#cuba").onClick.listen((e) => drawCuba(ctx));
+  querySelector("#china").onClick.listen((e) => drawCHINA(ctx));
+  querySelector("#button").onClick.listen((e) => clearCanvas());
+  querySelector("#San").onClick.listen((e) => drawSAN(ctx));
+  querySelector("#usa").onClick.listen((e) => drawUSA(ctx));
+  querySelector("#uk").onClick.listen((e) => drawUK(ctx));
+  querySelector("#canada").onClick.listen((e) => drawCanada(ctx));
 }
+  
+  drawStar(cx, cy, spikes, outerRadius, innerRadius) {
+    var rot = Math.pi / 2 * 3;
+    var x = cx;
+    var y = cy;
+    var step = Math.pi / spikes;
+
+    ctx.fillStyle = '#fff';
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius);
+    for (int i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy + Math.sin(rot) * outerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy + Math.sin(rot) * innerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+    }
+    ctx.lineTo(cx, cy - outerRadius);
+    ctx.closePath();
+    ctx.lineWidth=5;
+    ctx.strokeStyle='white';
+    ctx.stroke();
+    ctx.fillStyle='white';
+    ctx.fill();
+
+}
+  drawStarY(cx, cy, spikes, outerRadius, innerRadius) {
+    var rot = Math.pi / 2 * 3;
+    var x = cx;
+    var y = cy;
+    var step = Math.pi / spikes;
+
+    ctx.fillStyle = '#rgb(260,180,0)';
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius);
+    for (int i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy + Math.sin(rot) * outerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy + Math.sin(rot) * innerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+    }
+    ctx.lineTo(cx, cy - outerRadius);
+    ctx.closePath();
+    ctx.lineWidth=5;
+    ctx.strokeStyle='yellow';
+    ctx.stroke();
+    ctx.fillStyle='yellow';
+    ctx.fill();
+
+}
+
+  drawStarYT(cx, cy, spikes, outerRadius, innerRadius) {
+    var rot = Math.pi / 8 *1 ;
+    var x = cx;
+    var y = cy;
+    var step = Math.pi / spikes;
+
+    ctx.fillStyle = '#rgb(260,180,0)';
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius);
+    for (int i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy + Math.sin(rot) * outerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy + Math.sin(rot) * innerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+    }
+    ctx.lineTo(cx, cy - outerRadius);
+    ctx.closePath();
+    ctx.lineWidth=5;
+    ctx.strokeStyle='yellow';
+    ctx.stroke();
+    ctx.fillStyle='yellow';
+    ctx.fill();
+
+}
+
+ drawStarYT1(cx, cy, spikes, outerRadius, innerRadius) {
+    var rot = Math.pi / 4 *1 ;
+    var x = cx;
+    var y = cy;
+    var step = Math.pi / spikes;
+
+    ctx.fillStyle = '#rgb(260,180,0)';
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius);
+    for (int i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy + Math.sin(rot) * outerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy + Math.sin(rot) * innerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+    }
+    ctx.lineTo(cx, cy - outerRadius);
+    ctx.closePath();
+    ctx.lineWidth=5;
+    ctx.strokeStyle='yellow';
+    ctx.stroke();
+    ctx.fillStyle='yellow';
+    ctx.fill();
+
+}
+
+void drawCHINA(ctx){
+  //紅底
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  drawStarY(50, 50, 5, 20, 10);  
+  drawStarYT1(120, 25, 5, 5, 2.5); 
+  drawStarYT(135, 50, 5, 5, 2.5); 
+  drawStarY(135, 75, 5, 5, 2.5); 
+  drawStarYT1(120, 100, 5, 5, 2.5); 
+} 
+  void drawCuba(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(0, 0, 140)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(255, 255, 255)';
+  ctx.fillRect(0, 40, flag_w, flag_h/5);  
+  ctx.fillRect(0, 120, flag_w, flag_h/5);  
+  ctx.strokeStyle="rgb(255, 0, 0)";
+  ctx.stroke();  
+  ctx.moveTo(0,0);
+  ctx.lineTo(140,100);
+  ctx.lineTo(0,200);
+  ctx.fillStyle="rgb(255, 0, 0)";
+  ctx.fill();  
+  drawStar(50, 100, 5, 20, 10);
+  
+}
+void drawSAN(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(0, 200, 25)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  ctx.beginPath();
+  ctx.moveTo(0, 140);
+  ctx.lineTo(0, 200);
+  ctx.lineTo(60, 200);
+  ctx.lineTo(300, 60);
+  ctx.lineTo(300, 0);
+  ctx.lineTo(240, 0);
+  ctx.closePath();
+  ctx.fillStyle = 'rgb(240, 190, 0)';
+  ctx.fill();
+  ctx.beginPath();
+  ctx.moveTo(0, 160);
+  ctx.lineTo(0, 200);
+  ctx.lineTo(40, 200);
+  ctx.lineTo(300, 40);
+  ctx.lineTo(300, 0);
+  ctx.lineTo(260, 0);
+  ctx.closePath();
+  ctx.fillStyle = 'rgb(0, 0, 0)';
+  ctx.fill();
+  ctx.beginPath();
+  ctx.moveTo(60, 200);
+  ctx.lineTo(300, 200);
+  ctx.lineTo(300, 60);
+  ctx.closePath();
+  ctx.fillStyle = 'rgb(240, 0, 0)';
+  ctx.fill();
+  int x = 80;
+  int y = 145;
+  int r = 28;
+  bool solid = true;
+  var theta = 180;
+  var color = "white";
+  fivePointStar(x, y, r, solid, theta, color);
+  int a = 220;
+  int b = 55;
+  int c = 28;
+  bool solid1 = true;
+  var theta1 = 180;
+  var color1 = "white";
+  fivePointStar(a, b, c, solid1, theta1, color1);
+}
+
+void drawUSA(ctx){
+  //紅底
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  //白線
+  ctx.fillStyle = 'rgb(255, 255, 255)';
+  ctx.fillRect(0, 15.3, flag_w, flag_h/13);
+  ctx.fillRect(0, 45.9, flag_w, flag_h/13);
+  ctx.fillRect(0, 75.5, flag_w, flag_h/13);
+  ctx.fillRect(0, 106.1, flag_w, flag_h/13);
+  ctx.fillRect(0, 136.7, flag_w, flag_h/13);
+  ctx.fillRect(0, 167.3, flag_w, flag_h/13);
+  //籃底
+  ctx.fillStyle = 'rgb(0, 0, 140)';
+  ctx.fillRect(0, 0, 150,106.1 );
+  //星星
+  drawStar(14, 17, 5, 5, 2.5);
+  drawStar(38, 17, 5, 5, 2.5);
+  drawStar(62, 17, 5, 5, 2.5);
+  drawStar(88, 17, 5, 5, 2.5);
+  drawStar(112, 17, 5, 5, 2.5);
+  drawStar(136, 17, 5, 5, 2.5);
+  
+  drawStar(25, 36, 5, 5, 2.5);
+  drawStar(50, 36, 5, 5, 2.5);
+  drawStar(75, 36, 5, 5, 2.5);
+  drawStar(100, 36, 5, 5, 2.5);
+  drawStar(125, 36, 5, 5, 2.5);
+  
+  drawStar(14, 54, 5, 5, 2.5);
+  drawStar(38, 54, 5, 5, 2.5);
+  drawStar(62, 54, 5, 5, 2.5);
+  drawStar(88, 54, 5, 5, 2.5);
+  drawStar(112, 54, 5, 5, 2.5);
+  drawStar(136, 54, 5, 5, 2.5);
+  
+  drawStar(25, 71, 5, 5, 2.5);
+  drawStar(50, 71, 5, 5, 2.5);
+  drawStar(75, 71, 5, 5, 2.5);
+  drawStar(100, 71, 5, 5, 2.5);
+  drawStar(125, 71, 5, 5, 2.5);
+  
+  drawStar(14, 88, 5, 5, 2.5);
+  drawStar(38, 88, 5, 5, 2.5);
+  drawStar(62, 88, 5, 5, 2.5);
+  drawStar(88, 88, 5, 5, 2.5);
+  drawStar(112, 88, 5, 5, 2.5);
+  drawStar(136, 88, 5, 5, 2.5);
+} 
+
+void drawUK(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(0, 0, 140)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  ctx.strokeStyle="#fff";
+  ctx.lineWidth = 40;
+  ctx.beginPath();
+  ctx.moveTo(0,0);
+  ctx.lineTo(300,200);
+  ctx.moveTo(0,200);
+  ctx.lineTo(300,0);
+  ctx.stroke();
+  ctx.strokeStyle="#c9072a";
+  ctx.lineWidth = 20;
+  ctx.beginPath();
+  ctx.moveTo(0,205);
+  ctx.lineTo(150,105);
+  ctx.stroke();
+  ctx.strokeStyle="#c9072a";
+  ctx.lineWidth = 20;
+  ctx.beginPath();
+  ctx.moveTo(150,95);
+  ctx.lineTo(300,-5);
+  ctx.stroke();
+  ctx.strokeStyle="#c9072a";
+  ctx.lineWidth = 20;
+  ctx.beginPath();
+  ctx.moveTo(160,100);
+  ctx.lineTo(305,200);
+  ctx.stroke();
+  ctx.strokeStyle="#c9072a";
+  ctx.lineWidth = 20;
+  ctx.beginPath();
+  ctx.moveTo(140,100);
+  ctx.lineTo(-5,0);
+  ctx.stroke();
+  ctx.strokeStyle="#fff";
+  ctx.lineWidth = 40;
+    ctx.beginPath();
+    ctx.moveTo(0,100);
+    ctx.lineTo(300,100);
+    ctx.moveTo(150,0);
+    ctx.lineTo(150,200);
+    ctx.stroke();
+  ctx.strokeStyle="#c9072a";
+  ctx.lineWidth = 20;
+    ctx.beginPath();
+    ctx.moveTo(0,100);
+    ctx.lineTo(300,100);
+    ctx.moveTo(150,0);
+    ctx.lineTo(150,200);
+    ctx.stroke();
+
+  
+}
+
+void drawCanada(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(90, 0, 120,  200);
+  ctx.beginPath();
+  ctx.moveTo(147, 170);
+  ctx.lineTo(147, 140);
+  ctx.lineTo(125, 145);
+  ctx.lineTo(130, 135);
+  ctx.lineTo(95, 100);
+  ctx.lineTo(100, 95);
+  ctx.lineTo(102, 80);
+  ctx.lineTo(110, 85);
+  ctx.lineTo(115, 80);
+  ctx.lineTo(125, 90);
+  ctx.lineTo(123, 60);
+  ctx.lineTo(135, 70);
+  ctx.lineTo(150, 30);
+  ctx.lineTo(165, 70);
+  ctx.lineTo(177, 60);
+  ctx.lineTo(175, 90);
+  ctx.lineTo(185, 80);
+  ctx.lineTo(190, 85);
+  ctx.lineTo(198, 80);
+  ctx.lineTo(200, 95);
+  ctx.lineTo(205, 100);
+  ctx.lineTo(170, 135);
+  ctx.lineTo(175, 145);
+  ctx.lineTo(153, 140);
+  ctx.lineTo(153, 170);
+  ctx.lineTo(147, 170);
+  ctx.closePath();
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fill();
+  }
+
 void clearCanvas(){
-  ctx.clearRect(0, 0, flag_wi, flag_he);
+  ctx.clearRect(0, 0, flag_w, flag_h);
 }
